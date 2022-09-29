397734a104503006114f38545ad71683c9e14128
nnamaka <nnamaka7@gmail.com>
2022-09-29 22:55:38 +0100

    start creating cli for face detection

--- Computer_vision/face_Detection/faceapp_cli/faceid.py
+++ Computer_vision/face_Detection/faceapp_cli/faceid.py
@@ -1,12 +1,18 @@
 # dependencies
-import cv2 as cv
-import tensorflow as tf
-from layers import L1Dist
-import numpy as np
+# import cv2 as cv
+# import tensorflow as tf
+# from layers import L1Dist
+# import numpy as np
+import argparse
 
-# App layout
+my_parser = argparse.ArgumentParser(description='face recognition CLI')
 
+my_parser.add_argument('ID', metavar='id', type=str, help='starts verification')
 
+args = my_parser.parse_args()
 
-if __name__ == '__main__':
-    pass
+if ( args.ID == 'start'):
+    print("The program seems ok!")
+
+# if __name__ == '__main__':
+#     pass
